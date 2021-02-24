b67c3dceed135e608f182e7f0b1bf0b7965f196b
Mafe Bracho <mafe.bracho@gmail.com>
2021-02-24 21:01:05 +0100

    iteration done without linkedin logo

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import users from './users.json';
 
-class App extends React.Component {
+export default class App extends Component {
 
+  state = {
+    users: users,
+    firstName: "",
+    lastName: "",
+    campus: "",
+    role: "",
+    linkedin: ""
+  }
 
   render() {
-    (
-      < div ></div >
-    );
+    return (
+      <div className="App">
+        <h1>IronBook</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Campus</th>
+              <th>Role</th>
+              <th>Links</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.state.users.map(user => {
+              return (
+                <tr key={ user.id }>
+                  <td>{ user.firstName }</td>
+                  <td>{ user.lastName }</td>
+                  <td>{ user.campus }</td>
+                  <td>{ user.role }</td>
+                  <td>{ user.linkedin }</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    )
   }
 }
-
-export default App;
