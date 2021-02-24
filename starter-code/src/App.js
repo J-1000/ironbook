9f4a78bf2b88f2e8e91119fbfddd001588d6a7e7
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-02-24 15:56:44 +0100

    done with iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -8,11 +8,12 @@ class App extends React.Component {
   render() {
     const list = users.map((user,index) => {
       return (
-        <tr key={user.id} style={{display:'flex', alignItems: 'center', justifyContent: 'space-between'}}>
-          <p style={{margin: '10px', width: '20%'}}>{user.firstName}</p>
-          <p style={{margin: '10px', width: '20%'}}>{user.lastName}</p>
-          <p style={{margin: '10px', width: '20%'}}>{user.campus}</p>
-          <p style={{margin: '10px', width: '20%'}}>{user.role}</p>
+        <tr key={user.id} style={{width: '1000px', display:'flex', alignItems: 'center'}}>
+          <p style={{width: '200px'}}>{user.firstName}</p>
+          <p style={{width: '200px'}}>{user.lastName}</p>
+          <p style={{width: '200px'}}>{user.campus}</p>
+          <p style={{width: '200px'}}>{user.role}</p>
+          {user.linkedin && <a style={{width: '200px'}} href={user.linkedin}>linkedin</a>}
           {/* <button style={{width: '60px', height: '20px', margin: '20px'}}onClick={() => this.deleteHandler(index)}>Delete</button> */}
         </tr>
       )  
@@ -26,12 +27,12 @@ class App extends React.Component {
         alignItems: 'center'}}>
         <h1>IronBook</h1>
         <table>
-          <tr style={{display: 'flex', justifyContent: 'space-between', width: '800px'}}>
-            <th style={{paddingLeft: '10px'}}>First Name</th>
-            <th style={{paddingLeft: '50px'}}>Last Name</th>
-            <th style={{paddingLeft: '90px'}}>Campus</th>
-            <th style={{paddingLeft: '90px'}}>Role</th>
-            <th style={{paddingLeft: '50px'}}>Links</th>
+          <tr style={{tableLayout: 'fixed', display: 'flex', justifyContent: 'space-between', width: '1000px', textAlign: 'left'}}>
+            <th style={{width: '200px'}}>First Name</th>
+            <th style={{width: '200px'}}>Last Name</th>
+            <th style={{width: '200px'}}>Campus</th>
+            <th style={{width: '200px'}}>Role</th>
+            <th style={{width: '200px'}}>Links</th>
           </tr>
             {list}
         </table>
