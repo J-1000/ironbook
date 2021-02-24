cc5d75a228408562588e78e8354446039c520516
Aylin Ünal <ayl.uenal@gmail.com>
2021-02-24 21:10:49 +0100

    solve iteration 1 to display list with linkedin logo and link

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import './App.css';
+import users from "./users";
 
 class App extends React.Component {
 
 
   render() {
-    (
-      < div ></div >
+    console.log(users[0].firstName)
+    return(
+      <div>
+        <h1>IronBook</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Campus</th>
+              <th>Role</th>
+              <th>Links</th>
+              </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => {
+              return (
+
+            <tr key={index}>
+              <td>{ user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.campus}</td>
+              <td>{user.role}</td>
+                <td><a href={user.linkedin} target='blank'><img src={user.linkedin ? '/linkedin-logo.png' : ''} style={ {width: '20px'}}/></a></td>
+              </tr>
+
+              )
+            })
+            }
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
