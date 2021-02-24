33e684947a55fc8432fe3c49a913f6e2b87b10cc
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-02-24 16:26:07 +0100

    done with iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -5,8 +5,28 @@ import './App.css';
 
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        search: ''
+    }
+  }
+
+  handleSearch = event => {
+    const name = event.target.value;
+    console.log(name)
+    this.setState({
+      search: name
+    })
+  }
+
   render() {
-    const list = users.map((user,index) => {
+    const stateLowerCase = this.state.search.toLowerCase();
+    const filtered = users.filter(element => {
+      return (this.state.search === '' ? true: ( element.firstName.toLowerCase().includes(stateLowerCase)|| element.lastName.toLowerCase().includes(stateLowerCase)))
+    })
+    const list = filtered.map((user,index) => {
       return (
         <tr key={user.id} style={{width: '1000px', display:'flex', alignItems: 'center'}}>
           <p style={{width: '200px'}}>{user.firstName}</p>
@@ -14,7 +34,6 @@ class App extends React.Component {
           <p style={{width: '200px'}}>{user.campus}</p>
           <p style={{width: '200px'}}>{user.role}</p>
           {user.linkedin && <a style={{width: '200px'}} href={user.linkedin}>linkedin</a>}
-          {/* <button style={{width: '60px', height: '20px', margin: '20px'}}onClick={() => this.deleteHandler(index)}>Delete</button> */}
         </tr>
       )  
     })
@@ -26,6 +45,16 @@ class App extends React.Component {
         justifyContent: 'center',
         alignItems: 'center'}}>
         <h1>IronBook</h1>
+        <form onSubmit={this.handleSubmit} style={{width:'100%', marginBottom: '50px'}}>
+          <label htmlFor="search"></label>
+          <input style={{width:'80%', height: '25px', margin: '20px'}}
+            type="text"
+            name="search"
+            id="search"
+            value={this.state.search}
+            onChange={this.handleSearch}
+          />
+        </form>
         <table>
           <tr style={{tableLayout: 'fixed', display: 'flex', justifyContent: 'space-between', width: '1000px', textAlign: 'left'}}>
             <th style={{width: '200px'}}>First Name</th>
