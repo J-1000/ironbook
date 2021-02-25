c36d1787934eed6eefe8730162ba8c90f6f16b2e
Louisiane_Berlin <corentine.piette@gmail.com>
2021-02-25 15:31:54 +0100

    first commit

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,13 +1,88 @@
 import React from 'react';
 import './App.css';
+import users from "./users";
+import { v4 as uuidv4 } from 'uuid';
+
+const usersList = users
 
 class App extends React.Component {
 
+  state = {
+    search:'',
+    searchStudent:true,
+    searchTeacher:ture,
+    searchCountry:''
+  }
+
+  handleChange = event => {
+    const search = event.target.name
+    this.setState({
+      [search]: event.target.value
+    })
+
+  }
+
+  handleCheckbox = event => { 
+    this.setState({
+
+      searchStudent: event.target.checked
+    })
+  }
 
   render() {
-    (
-      < div ></div >
-    );
+
+    const option = [...]
+
+    const arrConc = usersList.map(user => ({...user, conc: user.firstName+user.lastName}))
+    console.log(this.state.searchStudent)
+    this.state.searchStudent ? let searchParam ="student"
+    const usersH = arrConc
+      .filter((filteredUser) => filteredUser.conc.includes(this.state.search))
+      
+      .map((user => {
+      return(
+        <tr key={uuidv4()}>
+        <td>{user.firstName}</td>
+        <td>{user.lastName}</td>
+        <td>{user.campus}</td>
+        <td>{user.role}</td>
+        {user.linkedin ? <td><a href={user.linkedin}>Linkedin</a></td> : <td></td>}
+        </tr>
+      )
+    }))
+
+
+    return (
+      <div>
+        <h1>IronBook</h1>
+        <input 
+        type="text" 
+        placeholder="Search.."
+        name="search"
+        id="search"
+        value={this.state.search}
+        onChange={this.handleChange}
+        ></input>
+        <input type="checkbox" id="checkbox" name="student" checked={this.state.searchStudent} onChange={this.handleCheckbox}></input>
+        <label htmlFor="radcheckboxio">Student</label>
+        <input type="checkbox" id="checkbox" name="teacher"></input>
+        <label htmlFor="checkbox">Teacher</label>
+        <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Campus</th>
+            <th>Role</th>
+            <th>Links</th>
+          </tr>
+        </thead>
+        <tbody>
+          {usersH}
+        </tbody>
+        </table>
+      </div>
+    )
   }
 }
 
