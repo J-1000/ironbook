cf36d0940019f27b023aa7c5e8717d460f6ee3cc
“Elvia <elvia.sicuro@gmail.com>
2021-02-25 14:40:48 +0100

    started iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -8,15 +8,33 @@ console.log(users)
 
 console.log(uuidv4())
 
-const linkedinPicture = './linkedin.png'
 
 
 class App extends React.Component {
-
   
+   state = {
+    search: ''
+
+   }
+
+
+   handleChange = event => {
+     //console.log(event.target.value)
+     //console.log(event.target.name)
+     const filteredUsers = users.filter(user => {
+      `${user.firstName}${user.lastName}`.toLowerCase().includes(this.state.search.toLowerCase())
+
+    })
+    this.setState({
+      search : event.target.value
+    })
+  }
+
 
   render() {
     //console.log(users)
+  
+    
      const usersList = users.map(user => {
           user.id = uuidv4()
        return (
@@ -25,13 +43,30 @@ class App extends React.Component {
            <td>{user.lastName}</td>
            <td>{user.campus}</td>
            <td>{user.role}</td>
-           <td><a href={user.linkedin}><img src={user.linkedin ? linkedinPicture : user.linkedin} /></a></td>
+           <td><a href={user.linkedin}><img src={user.linkedin ? '/linkedin.png' : ''}  alt="" style={{width: '25px'}} /></a></td>
          </tr>
        )
     })
+
+    // const filteredUsers = usersList.filter(user => {
+    //   `${user.firstName}${user.lastName}`.toLowerCase().includes(this.state.search.toLowerCase())
+
+    // })
+
+  
+    
     return (
       <div> 
       <h1>IronBook</h1>
+
+          <label htmlFor="search"> </label>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            value={this.state.search}
+            onChange={this.handleChange}
+          />
       <table>
       <thead>
           <tr>
