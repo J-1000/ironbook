91212d717bacfea942a39376b1b6110775df6881
Rahaf <r.abualhassan@gmail.com>
2021-09-22 11:42:04 +0200

    iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import './App.css';
+import users from "./users";
+import { v4 as uuid } from 'uuid';
+
+
+const usersList = users.map(user => {
+  return (
+  <tr key={ uuid()}>
+  <td>{user.firstName}</td>
+  <td>{user.lastName}</td>
+  <td>{user.campus}</td>
+  <td>{user.role}</td>
+  {user.linkedin && <td><a href={user.linkedin}><i className="fab fa-linkedin"></i></a></td>}
+  </tr>
+  )
+})
+
 
 class App extends React.Component {
 
 
   render() {
     return (
-      <div></div>
+      <div>
+        <header>
+          <h1>Ironbook</h1>
+        </header>
+        <div>
+        <table className="center">
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Campus</th>
+              <th>Role</th>
+              <th>Links</th>
+            </tr>
+          </thead>
+          <tbody>
+          {usersList}
+          </tbody>
+        </table>
+        </div>
+      </div>
     );
   }
 }
