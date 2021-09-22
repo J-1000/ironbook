07788e39d244b174551e5fcbd39ae8888ab92047
Marishka Zachariah <marishka.zachariah@gmail.com>
2021-09-22 12:02:04 +0200

    iterations 1 and 2 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,14 +1,62 @@
 import React from 'react';
 import './App.css';
+import users from "./users";
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+import linkedin from './linkedin.png';
 
-class App extends React.Component {
+function App () {
+    const [currentUsers, setCurrentUsers] = useState(users);
+    // const [firstName, setFirstName] = useState('');
+    // const [lastName, setLastName] = useState('');
+    const [searchName, setSearchName] = useState('');
+    
+    const filteredUsers = users.filter(user => {
+      return user.firstName.toLowerCase().includes(searchName.toLowerCase()) || user.lastName.toLowerCase().includes(searchName.toLowerCase())
+    })
 
+    const handleNameChange = event => {
+      setSearchName(event.target.value)
+      setCurrentUsers(filteredUsers);
+    }
 
-  render() {
     return (
-      <div></div>
-    );
-  }
+      <div>
+        <h1>Ironbook</h1>
+        <div>
+        <form>
+        <label htmlFor="search"></label>
+        <input type="search" name="search" id="search" value={searchName} onChange={handleNameChange} />
+        </form>
+        </div>
+        <div>
+          <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Campus</th>
+              <th>Roles</th>
+              <th>Links</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentUsers.map((user) => {
+              return (
+                <tr>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.campus}</td>
+                <td>{user.role}</td>
+                <td>{user.linkedin ? <img src={linkedin} alt="linkedin" width="40" height="40"/> : ""}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+          </table>
+        </div>
+      </div>
+    )
 }
 
 export default App;
