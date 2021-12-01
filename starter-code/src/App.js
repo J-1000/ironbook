fbfd058a030e6aba4b32815e524b80aa53e6e8bf
kostalexis89 <kostalexis.hosting@gmail.com>
2021-12-01 14:20:54 +0100

    Iteration 1 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react'
 import './App.css';
+import usersData from './users.json'
 
-class App extends React.Component {
-
-
-  render() {
-    return (
-      <div></div>
-    );
+// console.log(usersData)
+function App() {
+  
+  //*************USERS INITIALIZATION */
+  //Setting the initialize of the user state
+  const setInitialValue = () => {
+    return usersData
   }
+
+  //lets create a state
+  const [users, setUsers] = useState(() => setInitialValue())
+  //render some HTML with Javacript
+  console.log(users)
+  //creating a list of users ready to by displayed in the HTML
+  const usersList = users.map(user => {
+    return (
+      <tr>
+        <td>{user.firstName}</td>
+        <td>{user.lastName}</td>
+        <td>{user.campus}</td>
+        <td>{user.role}</td>
+        {user.linkedin && <td><a href={user.linkedin}>Linked In</a></td>}
+      </tr>
+    );
+  })
+
+    return (
+      <div className="App">
+      <h1>IronBook</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Campus</th>
+            <th>Role</th>
+            <th>Links</th>
+          </tr>
+        </thead>
+        <tbody>
+          {usersList}
+        </tbody>
+      </table>
+      
+    </div>
+    );
 }
 
 export default App;
