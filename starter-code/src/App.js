e780724a0315318583af6d6b58f68113474c00e0
Tsaikkari <kirsi.trospe@gmail.com>
2021-12-01 14:37:32 +0100

    Iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import './App.css';
+import users from "./users";
 
-class App extends React.Component {
-
-
-  render() {
-    return (
-      <div></div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Campus</th>
+              <th>Role</th>
+              <th>Links</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, i) =>
+              <tr key={i}>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.campus}</td>
+                <td>{user.role}</td>
+                <td>{user.linkedin}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+    </div>
+  );
 }
 
 export default App;
