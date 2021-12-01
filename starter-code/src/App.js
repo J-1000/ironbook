707a0ab3e60b039fcf46a1c214a2f1f629e1578f
kostalexis89 <kostalexis.hosting@gmail.com>
2021-12-01 17:48:29 +0100

    iteration 3 done!

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -14,12 +14,52 @@ function App() {
   //lets create a state
   const [users, setUsers] = useState(() => setInitialValue())
   const [search, setSearch] = useState('')
+  const [studentRole, setstudentRole] = useState(false)
+  const [teacherRole, setTeacherRole] = useState(false)
   //render some HTML with Javacript
   // console.log(users)
   //creating a list of users ready to by displayed in the HTML
-  const usersList = users.filter(function(user){
-    return user.firstName.toLowerCase().includes(search) || user.lastName.toLowerCase().includes(search)
-  }).map(user => {
+  
+
+
+
+  const handleSearchChange = event => {
+    // console.log('Key pressed')
+    setSearch(event.target.value)
+    
+  }
+
+  const handleStudentChange = () => {
+    // console.log('I am checking the check box')
+    if(!teacherRole){
+      setstudentRole(!studentRole)
+    }
+      
+
+  }
+  const handleTeacherChange= () => {
+    if(!studentRole){
+      setTeacherRole(!teacherRole)
+    }
+    
+  }
+
+  let results = users.filter(function(user){
+    return (user.firstName.toLowerCase().includes(search) || user.lastName.toLowerCase().includes(search))
+  })
+
+  if(studentRole){
+    results = results.filter(function(user){
+      return user.role==='student'
+    })
+  }
+  if(teacherRole){
+    results = results.filter(function(user){
+      return user.role==='teacher'
+    })
+  }
+
+  const usersList = results.map(user => {
     return (
       <tr>
         <td>{user.firstName}</td>
@@ -30,19 +70,14 @@ function App() {
       </tr>
     );
   })
-
-
-
-  const handleSearchChange = event => {
-    // console.log('Key pressed')
-    setSearch(event.target.value)
-    
-  }
-
   return (
   <div className="App">
     <h1>IronBook</h1>
     <input type='text' value={search} onChange={handleSearchChange} style={{width: "80vw", margin: "0 30px 50px 30px"}} />
+    <label htmlFor="studentRole">Student </label>
+      <input type="checkbox" name="studentRole" id="studentRole" checked={studentRole} onChange={handleStudentChange} />
+    <label htmlFor="teacherRole">Teacher </label>
+      <input type="checkbox" name="teacherRole" id="teacherRole" checked={teacherRole} onChange={handleTeacherChange} />
     <table id='table'>
        <thead>
          <tr>
