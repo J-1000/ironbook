2183333fd413f1e6e2af1f4bf225ae2f76c886e1
kostalexis89 <kostalexis.hosting@gmail.com>
2021-12-01 15:35:03 +0100

    Iteration 2 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -13,10 +13,13 @@ function App() {
 
   //lets create a state
   const [users, setUsers] = useState(() => setInitialValue())
+  const [search, setSearch] = useState('')
   //render some HTML with Javacript
-  console.log(users)
+  // console.log(users)
   //creating a list of users ready to by displayed in the HTML
-  const usersList = users.map(user => {
+  const usersList = users.filter(function(user){
+    return user.firstName.toLowerCase().includes(search) || user.lastName.toLowerCase().includes(search)
+  }).map(user => {
     return (
       <tr>
         <td>{user.firstName}</td>
@@ -28,25 +31,34 @@ function App() {
     );
   })
 
-    return (
-      <div className="App">
-      <h1>IronBook</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Campus</th>
-            <th>Role</th>
-            <th>Links</th>
-          </tr>
-        </thead>
-        <tbody>
-          {usersList}
-        </tbody>
-      </table>
-      
-    </div>
+
+
+  const handleSearchChange = event => {
+    // console.log('Key pressed')
+    setSearch(event.target.value)
+    
+  }
+
+  return (
+  <div className="App">
+    <h1>IronBook</h1>
+    <input type='text' value={search} onChange={handleSearchChange} style={{width: "80vw", margin: "0 30px 50px 30px"}} />
+    <table id='table'>
+       <thead>
+         <tr>
+           <th>First Name</th>
+           <th>Last Name</th>
+           <th>Campus</th>
+           <th>Role</th>
+           <th>Links</th>
+         </tr>
+       </thead>
+       <tbody>
+         {usersList}
+       </tbody>
+     </table>
+     
+  </div>
     );
 }
 
