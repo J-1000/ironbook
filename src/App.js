ce1acead7986fd3e3c9698082cc5179e2ec9bbea
Anna Morozova <a.morozova.nrw@gmail.com>
2021-12-02 10:43:15 +0100

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,45 +1,60 @@
 import React, { useState } from 'react'
 import './App.css';
 import data from './users.json'
+import logo from '../src/linkedin.png'
+import { render } from '@testing-library/react';
 
 function App() {
-  
+
+  //state for list of users
   const setInitialValue = () => {
     return data
   }
-
-  //create a state
   const [users, setUsers] = useState(() => setInitialValue())
- 
-  //creating a list of users
-  const usersList = users.map(user => {
+  //state for search bar
+  const [search, setSearch] = useState('')
+
+
+  //search bar
+  const handleSearch = event => {
+    setSearch(event.target.value)
+  }
+
+  let newSearch = users.filter((user) => {
+    return (user.firstName.toLowerCase() + user.lastName.toLowerCase()).includes(search)
+  })
+
   return (
-    <tr>
-      <td>{user.firstName}</td>
-      <td>{user.lastName}</td>
-      <td>{user.campus}</td>
-      <td>{user.role}</td>
-      {user.linkedin && <td><a href={user.linkedin}>LinkedIn</a></td>}
-    </tr>
-  );
-})
-  
-    return (
-      <div className="App">
+    <div className="App">
+      <form>
+        <input type='text' value={search} onChange={handleSearch} />
+
+      </form>
+
       <h1>IronBook</h1>
       <table class='center'>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Campus</th>
-            <th>Role</th>
-            <th>Links</th>
-          </tr>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Campus</th>
+          <th>Role</th>
+          <th>Links</th>
+        </tr>
         <tbody>
-          {usersList}
+          {newSearch.map(user => {
+            return (
+              <tr>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.campus}</td>
+                <td>{user.role}</td>
+                <td>{user.linkedin && <a href={user.linkedin}> <img src={logo} style={{ width: '15px' }} /></a>}</td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </div>
-    );
-    }
+  );
+}
 export default App;
