d273bb66446843b2baad7e5601512c3572b3e6d7
Alex Smith <axsmith2020@gmail.com>
2022-07-15 10:43:05 +0200

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,95 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import users from "./users";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [search, setSearch] = useState('')
+  const [teacher, setTeacher] = useState('false')
+  const [student, setStudent]= useState('false')
+  const [campus, setCampus] = useState()
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <form>
+        <input type="text" name="firstName" value={users.firstName} onChange={event =>{setSearch(event.target.value)}}  />
+      </form>
+      <label for="student">Student </label>
+      <input  type="checkbox" checked={student} onChange={event =>{setStudent(event.target.checked)}} ></input>
+      
+      <label for="teacher">Teacher </label>
+      <input  type="checkbox" checked={teacher} onChange={event =>{setTeacher(event.target.checked)}} ></input>
+      
+      <label for="campus">Campus: </label> 
+      <select onChange={event =>{setCampus(event.target.value)}}>
+        <option value="Tokyo">Tokyo</option>
+        <option value="Berlin">Berlin</option>
+        <option value="Lisbon">Lisbon</option>
+        <option value="Paris">Paris</option>
+        <option value="Amsterdam">Amsterdam</option>
+        <option value="Madrid">Madrid</option>
+        <option value="default" selected>Choose..</option>
+      </select>
+      <table>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Campus</th>
+          <th>Role</th>
+          <th>Links</th>
+        </tr>
+        {users.filter((val)=>{
+          if(search == ''){
+            return val
+          }
+          else if(val.firstName.toLowerCase().includes(search.toLowerCase())) {
+           return val
+          }
+        })
+        .filter((user) => {
+          if (student !== false) {
+            return user.role.includes('student')
+          } else {
+            return true
+          }
+        })
+        .filter((user) => {
+          if (teacher !== false) {
+            return user.role.includes('teacher')
+          } else {
+            return true
+          }
+        })
+        .filter((user) => {
+          if (campus === 'Tokyo') {
+            return user.campus.includes(campus)
+          } else if (campus === 'Berlin') {
+            return user.campus.includes(campus)
+          } else if (campus === 'Lisbon') {
+            return user.campus.includes(campus)
+          } else if (campus === 'Paris') {
+            return user.campus.includes(campus)
+          } else if (campus === 'Amsterdam') {
+            return user.campus.includes(campus)
+          } else if (campus === 'Madrid') {
+            return user.campus.includes(campus)
+          } 
+          else {
+            return user
+          }
+        })
+        .map(user => {
+          return(
+            <tr>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.campus}</td>
+              <td>{user.role}</td>
+              {user.linkedin && <th>LI</th>}
+              {!user.linkedin && <th> </th>}  
+            </tr>
+        )})}
+      </table>
     </div>
   )
 }
